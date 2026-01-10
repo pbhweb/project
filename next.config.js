77288ab466dd2fb2bf002d08fd730584cac47e02
pbhweb <150086520+pbhweb@users.noreply.github.com>
2026-01-10 09:05:43 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
-    domains: ['localhost', 'xkohlmtgycuumwmockdt.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xkohlmtgycuumwmockdt.supabase.co",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
+    unoptimized: true, // <UPDATE> Added unoptimized option
   },
   experimental: {
-    serverActions: true,
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
+  typescript: {
+    ignoreBuildErrors: true, // <UPDATE> Added ignoreBuildErrors option
   },
 }
-
 
 module.exports = nextConfig
